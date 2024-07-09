5ebdf05594d83e7133d5e6e97b07921ebf4f33c4
Jatin Pant <kineticenergy96@outlook.com>
2024-07-09 16:35:16 +0530

    implemented populate and fitness functions

--- src/index.js
+++ src/index.js
@@ -18,23 +18,50 @@ const TARGET = "JATIN PANT"
 
 const population = []
 
-const populate = (no_of_indices) => {
-    for (let i=0; i<no_of_indices; i++) {
-        for (let j=0; j<TARGET.length; j++) {
-            let item = ""
+const get_random_letter = (str) => {
+    let len = TARGET.length
+    const rand_no = Math.floor(Math.random() * len)
+
+    return str[rand_no]
+}
+
+const populate = (no_of_indices = 20) => {
+    for (let i = 0; i < no_of_indices; i++) {
+        let item = {}
+        let label = ""
+        for (let j = 0; j < TARGET.length; j++) {
             let letter = get_random_letter(ALPHABET)
+            label += letter
         }
+        item.label = label
+        item.fitness = get_fitness(label, ALPHABET)
+        population.push(item)
     }
 }
 
 const get_fitness = (check, target) => {
     let fitness = 0
-    
-    for (let i=0; i<check.length; i++) {
+
+    for (let i = 0; i < check.length; i++) {
         if (check.charAt(i) === target.charAt(i))
             fitness++
     }
     return fitness
 }
+populate()
+let newPopulation = population.slice()
+newPopulation.sort((item1, item2) => item2.fitness - item1.fitness)
 
-console.log(get_fitness("AETDSYWHDD", TARGET))
+/**
+ * @function
+ * selection
+ * @explanation
+ * Take the top 10% of the fittest as the same in the new population.
+ * Crossover the top 50% with the remaining 50%
+ * Take the next 40% of the bottom
+ * Mutate
+ */
+
+const select = () => {
+
+}
