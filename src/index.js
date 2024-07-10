f42fd437ea0806df648e7e129283f99d5ec9e49a
Jatin Pant <kineticenergy96@outlook.com>
2024-07-10 11:29:42 +0530

    set tye type: module in package.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { populate } from "./populate.js";
 /**
  * @author Jatin-Pant<https://github.com/notjatin>
  * @explanation
@@ -12,45 +13,17 @@
  * @param {string} : alphabet sequence
  */
 
-const ALPHABET = "ABCDEFGHIJKLMNOPQUSTUVWXYZ "
+const ALPHABET = "ABCDEFGHIJKLMNOPQUSTUVWXYZ ";
 
-const TARGET = "JATIN PANT"
+const TARGET = "JATIN PANT";
 
-const population = []
+let population = [];
 
-const get_random_letter = (str) => {
-    let len = TARGET.length
-    const rand_no = Math.floor(Math.random() * len)
 
-    return str[rand_no]
-}
 
-const populate = (no_of_indices = 20) => {
-    for (let i = 0; i < no_of_indices; i++) {
-        let item = {}
-        let label = ""
-        for (let j = 0; j < TARGET.length; j++) {
-            let letter = get_random_letter(ALPHABET)
-            label += letter
-        }
-        item.label = label
-        item.fitness = get_fitness(label, ALPHABET)
-        population.push(item)
-    }
-}
 
-const get_fitness = (check, target) => {
-    let fitness = 0
-
-    for (let i = 0; i < check.length; i++) {
-        if (check.charAt(i) === target.charAt(i))
-            fitness++
-    }
-    return fitness
-}
-populate()
-let newPopulation = population.slice()
-newPopulation.sort((item1, item2) => item2.fitness - item1.fitness)
+// let newPopulation = population.slice()
+// newPopulation.sort((item1, item2) => item2.fitness - item1.fitness)
 
 /**
  * @function
@@ -62,6 +35,51 @@ newPopulation.sort((item1, item2) => item2.fitness - item1.fitness)
  * Mutate
  */
 
-const select = () => {
-
+const crossover = (st1, st2) => {
+    let len = st1.length
+    // if (len !== st2.length) return
+    let newStr = ""
+    for (let i=0; i<len; i++) {
+        if (i<len/2)
+            newStr[i] = st1[i]
+        else
+        newStr[i] = st2[i]
+    }
+    return newStr
 }
+
+const select = () => {
+    console.log("select called")
+  const sortedPopulation = population
+    .slice()
+    .sort((item1, item2) => item2 - item1);
+  const newPopulation = new Array(population.length);
+  for (let i = 0; i < population.length; i++) {
+    if (i<50) {
+        newPopulation[i] = crossover(population[i], population[50+i])
+    } else if (i<60) {
+        newPopulation[i] = sortedPopulation[i-50]
+    } else {
+        newPopulation[i] = sortedPopulation[i]
+    }
+  }
+  population = newPopulation.slice()
+};
+
+const printPopulation = (population) => {
+    console.log("Printing new population")
+    for (let i=0; i<population.length; i++) {
+        console.log(`Label: ${population[i].label} Fitness: ${population[i].fitness}`)
+    }
+}
+
+/**
+ * Looping over 10 generations
+ */
+let gen = 0;
+populate(population, 100, ALPHABET, TARGET)
+// while (gen < 10) {
+//     printPopulation(population)
+//     select()
+//     gen++
+// }
