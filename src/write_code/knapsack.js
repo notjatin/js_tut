8d4d66bce78695d83b465cf160e34c72ae2f815d
Jatin Pant <kineticenergy96@outlook.com>
2024-07-14 21:23:24 +0530

    added problem discription

---
+++ src/write_code/knapsack.js
@@ -0,0 +1,7 @@
+/**
+ * A thief robbing a store finds n items. 
+ * The i'th item is worth v(i) dollars and weights w(i) pounds, 
+ * where v(i) and w(i) are integers. 
+ * The thief wants to take as valuable a load as possible,
+ * but he can carry at most W pounds in his knapsack, integer W.
+ */
